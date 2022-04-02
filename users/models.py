6c08567c03d5f0ec5567d97c3c4a453e06aeed42
Lisa <lisalis@vivalde.net>
2022-04-02 20:43:34 +0300

    update model USER

--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom User Model
+    """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_UKRAINIAN = "uk"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_UKRAINIAN, "Ukrainian"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_UAH = "uah"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_UAH, "UAH"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, blank=True, max_length=10, null=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthday = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, blank=True, max_length=2)
+    currency = models.CharField(choices=CURRENCY_CHOICES, blank=True, max_length=3)
+    superhost = models.BooleanField(default=False)
