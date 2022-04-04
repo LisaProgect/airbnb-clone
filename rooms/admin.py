1921f561c2837f8f934a2cf680653a6a66f82e6d
Lisa <lisalis@vivalde.net>
2022-04-04 22:28:48 +0300

    creating dependencies for model Room

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, RoomType, Facility, HouseRule, Amenity, Photo
+
+
+@admin.register(RoomType, Facility, HouseRule, Amenity)
+class ItemAdmin(admin.ModelAdmin):
+    """Item Admin Definition"""
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """Photo Admin Definition"""
 
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    """Room Admin"""
+    """Room Admin Definition"""
