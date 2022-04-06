339b74392af0f132f0fc4f26ebd617b91b583e10
Lisa <lisalis@vivalde.net>
2022-04-06 21:10:15 +0300

    add average

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,3 +5,8 @@ from .models import Review
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     """Review Admin Definition"""
+
+    list_display = (
+        "__str__",
+        "rating_average",
+    )
