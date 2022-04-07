b9a382abd0ebab7a55aa8404a3363168161c6c9e
Lisa <lisalis@vivalde.net>
2022-04-07 15:26:26 +0300

    change admin panel for model "Lists"

--- lists/admin.py
+++ lists/admin.py
@@ -5,3 +5,11 @@ from .models import List
 @admin.register(List)
 class ListAdmin(admin.ModelAdmin):
     """List Admin Definition"""
+
+    list_display = (
+        "name",
+        "user",
+        "count_rooms",
+    )
+
+    search_fields = ("name",)
